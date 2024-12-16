6e96394f952e43f7638732aef799d668a1be8b43
omar switi <omarswiti10@gmail.com>
2024-12-16 23:13:41 +0200

    assignment26

---
+++ assignment26/creature.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assignment26
+{
+
+    public class Creature
+    {
+        public virtual void Speak()
+        {
+            Debug.Log("sound.");
+        }
+    }
+}
