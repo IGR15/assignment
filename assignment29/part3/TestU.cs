5b8c57558812bf2f29680f7652ad3f426706d45e
omar switi <omarswiti10@gmail.com>
2025-01-17 21:12:11 +0200

    rigid

--- assignment29/part3/TestU.cs
+++ assignment29/part3/TestU.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace Assignment29{
-public class TestU : MonoBehaviour
+namespace Assignment29
 {
-    // Start is called before the first frame update
-    void Start()
+    public class TestU : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
         {
             Debug.Log(Utilities.Add(1, 2, 3, 4, 5));
 
             string repeated = "Hello".RepeatString(3);
             Debug.Log(repeated);
         }
-}
+    }
 }
